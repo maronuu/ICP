76dbb20431c51bf47c024c5802d34f30a578f034
maronuu <oguri.ytr.2000@gmail.com>
2022-02-15 08:43:45 +0900

    sample data generation, random shift, brute-force algo

--- include/icp.hpp
+++ include/icp.hpp
@@ -1,3 +1,9 @@
 #pragma once
 
-void icp();
+#include <eigen3/Eigen/Dense>
+
+void generate_point_cloud(const int num_sample, const double radius, Eigen::MatrixXd &data);
+
+void random_shift(const Eigen::MatrixXd &src, Eigen::MatrixXd &dst);
+
+void icp(const Eigen::MatrixXd &data1, const Eigen::MatrixXd &data2, std::vector<int> &correspondence);
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <vector>
 
 #include "icp.hpp"
 
-int main() {
-    std::cout << "ICP START" << std::endl;
-    icp();
-    std::cout << "ICP END" << std::endl;
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./main <num_sampling_points>" << std::endl;
+        return EXIT_FAILURE;
+    }
+    const int num_sample = atoi(argv[1]);
+    const double radius = 10.0;
+
+    // generate sample data
+    Eigen::MatrixXd data1 = Eigen::MatrixXd::Zero(num_sample, 2);
+    generate_point_cloud(num_sample, radius, data1);
+
+    // shift and rotate at random
+    Eigen::MatrixXd data2 = Eigen::MatrixXd::Zero(num_sample, 2);
+    random_shift(data1, data2);
+
+    // run icp
+    std::vector<int> correspondence(num_sample, -1);
+    icp(data1, data2, correspondence);
+
+    // plot
+    for (int i = 0; i < num_sample; ++i)
+    {
+        std::cout << i << ": " << i << " -> " << correspondence[i] << std::endl;
+    }
 }
