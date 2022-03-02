2f26a5f4e95f809bebb4f4adcd1140cc4fd9aac3
maronuu <oguri.ytr.2000@gmail.com>
2022-03-02 19:31:58 +0900

    added eigen & googletest

--- install.sh
+++ install.sh
@@ -5,3 +5,20 @@ cd build
 cmake ..
 cmake --build .
 cmake --install . --prefix ../../glfwlib
+
+git submodule update external/eigen
+cd external/eigen
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cmake --install . --prefix ../../eigenlib
+
+git submodule update external/googletest
+cd external/googletest
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cmake --install . --prefix ../../googletestlib
+
